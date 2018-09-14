7b77a17d5a66af3cac1c1ada6cfa560c97cbc7dd
SHAJ <shakilahmedshaj@gmail.com>
2018-09-14 10:58:47 +0600

    toast added

--- app/src/main/java/com/t3ch/shaj/android_listview_filtering_searchview/MainActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_listview_filtering_searchview/MainActivity.java
@@ -2,12 +2,16 @@ package com.t3ch.shaj.android_listview_filtering_searchview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +20,22 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.ListView_ID);
 
-        String[] countryName = getResources().getStringArray(R.array.countries_names);
+        final String[] countryName = getResources().getStringArray(R.array.countries_names);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.base_view, R.id.TV_ID, countryName);
+        adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.base_view, R.id.TV_ID, countryName);
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = countryName[position];
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
     }
 }
